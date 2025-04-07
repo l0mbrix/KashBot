c7b43305835333a0a0bfc526a29c61b25e3cd9e6
l0mbrix <agathe.vandekerckhove@gmail.com>
2025-04-07 13:45:42 +0200

    Test séparation var 4

--- enum/matchWordsList.js
+++ enum/matchWordsList.js
@@ -1,2 +1,20 @@
 // Here is the banned word list.
-const sorryWordsList = ['désolé', 'désolée', 'déso', 'dsl', 'sorry', 'sry', 'mea culpa', 'mea maxima culpa', 'm\'excuse', 'm\'excuser', 'excuse', 'pardon', 'pardonnez', 'mb', 'my bad'];
+const sorryWordsList = [
+    'désolé', 
+    'désolée', 
+    'déso', 
+    'dsl', 
+    'sorry', 
+    'sry', 
+    'mea culpa', 
+    'mea maxima culpa', 
+    'm\'excuse', 
+    'm\'excuser', 
+    'excuse', 
+    'pardon', 
+    'pardonnez', 
+    'mb', 
+    'my bad'
+];
+
+module.exports = { sorryWordsList };
--- enum/randomReply.js
+++ enum/randomReply.js
@@ -1,7 +1,9 @@
 // Here you can set up the random sentences your bot will reply to users infringing your rules (matchWordsList).
-randomSorryReply = [
+const randomSorryReply = [
     `💸 Et hop ! 1€ de plus dans la cagnotte`, 
     `😏 Ben alors **${message.author.username}**, on s\'excuse encore ?`,
     `💰 Dis-donc ! On n\'avait pas dit qu\'on ne s\'excusait plus ici ?`,
     `🪙 ALERTE CONTRIBUTION ! **${message.author.username}** vient d\'ajouter 1 nouvel € dans la boite !`
-]
+];
+
+module.exports = { randomSorryReply };
