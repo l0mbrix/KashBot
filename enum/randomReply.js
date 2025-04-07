612f5c3c2772954507c1543004fe26c5f875d375
l0mbrix <agathe.vandekerckhove@gmail.com>
2025-04-07 17:01:39 +0200

    Test séparation var 6

--- enum/randomReply.js
+++ enum/randomReply.js
@@ -1,9 +1,9 @@
 // Here you can set up the random sentences your bot will reply to users infringing your rules (matchWordsList).
-const randomSorryReply = [
+
+module.exports.randomSorryReply = [
+    // Fonction qui acceptent message et retourne une réponse
     (message) => `💸 Et hop ! 1€ de plus dans la cagnotte`, 
     (message) => `😏 Ben alors **${message.author.username}**, on s\'excuse encore ?`,
     (message) => `💰 Dis-donc ! On n\'avait pas dit qu\'on ne s\'excusait plus ici ?`,
     (message) => `🪙 ALERTE CONTRIBUTION ! **${message.author.username}** vient d\'ajouter 1 nouvel € dans la boite !`
 ];
-
-module.exports = { randomSorryReply };
